b6433220413b05d4d6177ea4564b4e9deeaa2362
Gabriel Marks <gabriel_marks@brown.edu>
2020-03-05 11:45:45 -0500

    Organize code

--- tophat.py
+++ tophat.py
@@ -5,27 +5,34 @@ from selenium.webdriver.common.by import By
 from pynotifier import Notification
 import time
 
-true_url = input("Enter url: ").strip()
-if true_url.find('://'):
-    true_url = true_url[true_url.find('://') + 3 :]
-if true_url.endswith('/'):
-    true_url = true_url[:-1]
+LONG_WAIT = 10**12
 
+def get_driver(data_dir):
+    options = webdriver.ChromeOptions()
+    options.add_argument('user-data-dir=' + data_dir)
+    return webdriver.Chrome(options=options)
 
-options = webdriver.ChromeOptions()
-options.add_argument('user-data-dir=/home/gmarks/.config/google-chrome-2/')
+def load_noredirect(driver, url):
+    driver.get('https://' + true_url)
+    WebDriverWait(driver, LONG_WAIT).until(EC.url_contains(true_url))
 
-driver = webdriver.Chrome(options=options)
-driver.get('https://' + true_url)
+if __name__ == '__main__':
+    true_url = input("Enter url: ").strip()
+    if true_url.find('://'):
+        true_url = true_url[true_url.find('://') + 3 :]
+    if true_url.endswith('/'):
+        true_url = true_url[:-1]
 
-WebDriverWait(driver, 10000000).until(EC.url_contains(true_url))
-print('on page')
-while 1:
-    WebDriverWait(driver, 10000000000).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'list-row--answered')]")))
-    print('found')
-    Notification(title='New question spotted!',
-                 description=true_url,
-                 duration=60,
-                 urgency=Notification.URGENCY_CRITICAL).send()
-    time.sleep(30)
+    driver = get_driver('/home/gmarks/.config/google-chrome-2/')
+    load_noredirect(driver, true_url)
+    print('page loaded')
+    
+    while 1:
+        WebDriverWait(driver, LONG_WAIT).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'list-row--answered')]")))
+        print('new q found')
+        Notification(title='New question spotted!',
+                     description=true_url,
+                     duration=60,
+                     urgency=Notification.URGENCY_CRITICAL).send()
+        time.sleep(30)
                                         
